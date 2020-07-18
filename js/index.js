a02674c64abcb47ca334ebc2a3a9f8ac12c624d9
minhnnguyen003 <minhnnguyen003@gmail.com>
2020-07-18 17:18:31 +0700

    18/7/2020 5:18 PM

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-// document.getElementById(inlineFormCustomSelect).addEventListener('change', appUI.insert_2nd_list)
 
-// const appUI = {
-//     insert_2nd_list : function {
-//         let val = document.getElementById(inlineFormCustomSelect).value;
-        
-//     }
-// }
+const appUI = {
+    insert_2nd_list : function () {
+        let val = document.getElementById('inlineFormCustomSelect').value;
+    },
+}
+
+document.getElementById('inlineFormCustomSelect').addEventListener('change', appUI.insert_2nd_list)
