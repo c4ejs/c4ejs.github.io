da9b47227a7d440c1d6d00e2f544c3b158e9c068
thanh97bn <thanh.le.ktmm@gmail.com>
2020-07-26 12:08:39 +0700

    Thành merge code 26/07/2020

---
+++ js/logInAndlRegister.js
@@ -0,0 +1,13 @@
+function saveInfoUserAndPassStore() {
+    const usernameRegis = document.querySelector('.username-register');
+    if (usernameRegis < )
+        localStorage.setItem('username', usernameRegis.value);
+}
+const loginForTravel = function() {
+    const userName = document.forms('.usernam-log');
+    const password = document.querySelector('.password-login');
+}
+
+//Tạo sự kiện đăng nhập
+const btnLogin = document.querySelector('.btnLogin');
+btnLogin.addEventListener('click', loginForTravel);
